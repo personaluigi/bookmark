b058c932a2cbd9258b6defa46e90bc888d8e387a
Luigi Armani <133150085+personaluigi@users.noreply.github.com>
2023-12-02 15:17:59 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -11,18 +11,18 @@ btnMenu.addEventListener('click', () => {
 
 const features = [
     {   
-        img: "../images/illustration-features-tab-1.svg",
+        img: "./images/illustration-features-tab-1.svg",
         h3: "Bookmark in one click",
         p: "Organize your bookmarks however you like. Our simple drag-and-drop interface gives you complete control over how you manage your favourite sites."
 
     },
     {
-        img: "../images/illustration-features-tab-2.svg",
+        img: "./images/illustration-features-tab-2.svg",
         h3: "Intelligent search",
         p: "Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks."
     },
     {
-        img: "../images/illustration-features-tab-3.svg",
+        img: "./images/illustration-features-tab-3.svg",
         h3: "Share your bookmarks",
         p: "Easily share your bookmarks and collections with others. Create a shareable link that you can send at the click of a button."
     }
